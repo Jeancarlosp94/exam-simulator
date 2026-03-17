01cfb2c627d2a69c5bc752e592df2a987081a72c
Jeancarlo Ponce <jcp9413@gmail.com>
2026-03-16 22:00:41 -0500

    Corregido error de repositorio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/exam-simulator/"
+  base: "https://github.com/Jeancarlosp94/exam-simulator"
 })
